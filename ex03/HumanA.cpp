cfb7068447072f81d40f2bc10b29d0da9ea8a041
Tigran Sargsyan <tigran.sargsyan.w@gmail.com>
2025-12-10 14:54:59 +0100

    add: add Doxygen comments to HumanA, HumanB, Weapon classes and main function

--- ex03/HumanA.cpp
+++ ex03/HumanA.cpp
@@ -1,14 +1,28 @@
 #include "HumanA.hpp"
 #include <iostream>
 
+/**
+ * @brief Constructs a HumanA object with a name and a weapon.
+ * @param name The name of the HumanA.
+ * @param weapon A reference to the weapon.
+ */
 HumanA::HumanA(const std::string &name, Weapon &weapon)
 	: name(name),
 		weapon(weapon) {}
 
+/**
+ * @brief Constructs a HumanA by copying another HumanA.
+ * @param other The other HumanA to copy.
+ */
 HumanA::HumanA(const HumanA &other)
 	: name(other.name),
 		weapon(other.weapon) {}
 
+/**
+ * @brief Assigns another HumanA to this one.
+ * @param other The other HumanA to assign from.
+ * @return A reference to this HumanA.
+ */
 HumanA &HumanA::operator=(const HumanA &other)
 {
 	if (this != &other)
@@ -21,8 +35,14 @@ HumanA &HumanA::operator=(const HumanA &other)
 	return (*this);
 }
 
+/**
+ * @brief Destroys the HumanA object.
+ */
 HumanA::~HumanA() {}
 
+/**
+ * @brief Makes the HumanA attack with their weapon.
+ */
 void HumanA::attack() const
 {
 	std::cout << name
